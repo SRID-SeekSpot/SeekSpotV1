450b8e9593d1ef89bedf997e4dc69f0d43b79be9
Kejie He <kejieh@andrew.cmu.edu>
2023-11-23 12:45:41 -0800

    Homepage Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,66 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+import LogoSquare from "../public/LogoSquare.png";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
+    const locationOptions = [
+        { value: "Vista Valley Mall", label: "Vista Valley Mall" },
+        { value: "Spectrum Square", label: "Spectrum Square" },
+        { value: "West Marketplace", label: "West Marketplace" },
+    ];
+
     return (
-        <div>
+        <div className="bg-sky h-screen">
             <title>SRID HomePage</title>
-            <p className="text-3xl">
-                This is the SeekSpot Project for SRID course
-            </p>
-            <Button asChild variant={"default"}>
-                <a href="/styleguide">StyleGuide Definition</a>
-            </Button>
+            <div className="flex flex-col justify-center items-center bg-sky">
+                {/* Logo Panel */}
+                <div className="mt-36">
+                    <Image alt="mainLogo" src={LogoSquare} width={320}></Image>
+                </div>
+                {/* Location Selection Panel */}
+                <div className="flex flex-row mt-20 justify-center items-center">
+                    <p>Location: </p>
+                    <div className="ml-10">
+                        <Select>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Select a location" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>Select a Location</SelectLabel>
+                                    {locationOptions.map((location) => {
+                                        return (
+                                            <SelectItem value={location.value}>
+                                                {location.label}
+                                            </SelectItem>
+                                        );
+                                    })}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </div>
+                </div>
+                {/* Button Panel */}
+                <div className="flex flex-col mt-20 justify-between space-y-4">
+                    <Button className="w-56">Specialist</Button>
+                    <Button className="w-56" variant={"secondary"}>Non-Specialist</Button>
+                    <Button className="w-56" variant={"outline"} asChild>
+                        <a href="/styleguide">Style Guide</a>
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 }
