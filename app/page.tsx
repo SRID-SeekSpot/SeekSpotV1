319d1f4e4ab8837df30fbe2816be16d7e5e433bd
Kejie He <kejieh@andrew.cmu.edu>
2023-11-23 09:28:52 -0800

    Project Template

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div>
+            <p>This is the SeekSpot Project for SRID course</p>
+        </div>
+    );
+}
