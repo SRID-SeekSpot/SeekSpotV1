f86cdc02c87d087af94f1448070333991ea5e3f1
Kejie He <kejieh@andrew.cmu.edu>
2023-11-23 22:52:22 -0800

    navbar

--- app/specialistdb/page.tsx
+++ app/specialistdb/page.tsx
@@ -5,14 +5,26 @@ import Image from "next/image";
 import LogoFlat from "../../public/LogoFlat.png";
 import { Separator } from "@/components/ui/separator";
 import Header from "@/components/general/header";
+import Navbar from "@/components/general/navbar";
 
 export default function Home() {
+    // Icon Src and Route for Navigation Bar
+    const navButtons = [
+        { src: "/icons/IconHome.png", alt: "Home", route: "/specialistdb" },
+        { src: "/icons/IconMoney.png", alt: "BountyHunt", route: "/profile" },
+        { src: "/icons/IconSearch.png", alt: "Search", route: "/messages" },
+        { src: "/icons/IconAvatar.png", alt: "Settings", route: "/settings" },
+    ];
+
     return (
         <div className="flex flex-col">
             <title>Specialist Dashboard</title>
             {/* Header that redirects to home */}
             {/* <Header href="/" altText="Hello"/> */}
-            <Header href="/"/>
+            <Header href="/" />
+
+            {/* Bottom Navigation Bar */}
+            <Navbar navButtons={navButtons} />
         </div>
     );
 }
---
+++ components/general/navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Separator } from "../ui/separator";
+
+// Define the type for the button
+interface NavButton {
+    src: string;
+    alt: string;
+    route: string;
+}
+
+// Define the props for the Navbar component
+interface NavbarProps {
+    navButtons: NavButton[]; // Array of NavButton objects
+}
+
+// Navbar component with props
+const Navbar: React.FC<NavbarProps> = ({ navButtons }) => {
+    return (
+        <div className="fixed bottom-0 left-0 right-0">
+            <Separator />
+            <div className="bg-white shadow-lg flex justify-around items-center h-16">
+                {navButtons.map((button, index) => (
+                    <Link key={index} href={button.route} passHref>
+                        <Image
+                            src={button.src}
+                            alt={button.alt}
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
