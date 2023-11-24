05b2e22bf78029cf395268c40d41d393d78bf9d7
Kejie He <kejieh@andrew.cmu.edu>
2023-11-23 23:05:04 -0800

    Dashboard

--- app/specialistdb/page.tsx
+++ app/specialistdb/page.tsx
@@ -22,7 +22,44 @@ export default function Home() {
             {/* Header that redirects to home */}
             {/* <Header href="/" altText="Hello"/> */}
             <Header href="/" />
-
+            {/* Body */}
+            <div className="flex flex-col">
+                {/* Bounty Hunt List */}
+                <div className="flex flex-col m-4">
+                    <div className="flex flex-row justify-start items-baseline">
+                        <Image
+                            src="/icons/IconMoney.png"
+                            alt="BountyHunt"
+                            width={25}
+                            height={25}
+                        ></Image>
+                        <p className="text-3xl ml-2">Bounty Hunt List</p>
+                    </div>
+                    <div className="mt-2">
+                        <p className="text-sm">
+                            List of items people have lost. Help them find their
+                            treasures and gain a reward!
+                        </p>
+                    </div>
+                </div>
+                {/* Found Item Inventory */}
+                <div className="flex flex-col m-4">
+                    <div className="flex flex-row justify-start items-baseline">
+                        <Image
+                            src="/icons/IconSearch.png"
+                            alt="FoundItem"
+                            width={25}
+                            height={25}
+                        ></Image>
+                        <p className="text-3xl ml-2">Found Item Inventory</p>
+                    </div>
+                    <div className="mt-2">
+                        <p className="text-sm">
+                        List of items stored in the Lost & Found office. Come and see if your item is here!
+                        </p>
+                    </div>
+                </div>
+            </div>
             {/* Bottom Navigation Bar */}
             <Navbar navButtons={navButtons} />
         </div>
