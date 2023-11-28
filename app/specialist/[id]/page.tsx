4395b2cb9fc3b0895d21b7ebdea40be87bc3679e
yolandajhzm <jianghuizhuoma@gmail.com>
2023-11-27 20:48:46 -0800

    add found and lost item detail page for both specialist and non-specialist

--- app/specialist/[id]/page.tsx
+++ app/specialist/[id]/page.tsx
@@ -2,21 +2,26 @@
 
 import { usePathname } from 'next/navigation'
 import { BOUNTY_ITEMS } from '@/app/constants/BountyItems';
-import ItemDetailPage from '@/app/specialist/itemDetail/page';
+import FoundItemDetailPage from '@/app/specialist/foundItemDetail/page';
+import LostItemDetailPage from '@/app/specialist/lostItemDetail/page';
 
 const ItemDetail = () => {
     const path = usePathname();
     const id = path.split("/").pop();
-    
     const item = BOUNTY_ITEMS.find((item) => item.id === id);
-    console.log(item);
 
     // If item is not found
-    if (!item) {
+    if (!item || !id) {
         return <p>Item not found!</p>;
     }
 
-    return <ItemDetailPage item={item} />;
+    if (id.startsWith("f")) {
+        return <FoundItemDetailPage item={item} />;
+    } else if (id.startsWith("l")) {
+        return <LostItemDetailPage item={item} />;
+    } else {
+        return <p>Invalid item ID!</p>;
+    }
 };
 
 export default ItemDetail;
