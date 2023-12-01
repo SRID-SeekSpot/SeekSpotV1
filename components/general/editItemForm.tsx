47c79226a155012a3b385e3bca03178b1a934217
Haoxin Lin <haoxinlin07@gmail.com>
2023-12-01 15:47:50 -0800

    edit

--- components/general/editItemForm.tsx
+++ components/general/editItemForm.tsx
@@ -67,7 +67,7 @@ const PopupDelete = ({ onClose }) => {
 
 const PopupCongratulations = ({onClose}) => {
 
-    const popupCongratulationsFailStyle = {
+    const popupCongratulationsStyle = {
       position: 'fixed',
       top: 0,
       left: 0,
@@ -95,7 +95,7 @@ const PopupCongratulations = ({onClose}) => {
 
     return (
       <div>
-        <div className="popup-container" style={popupCongratulationsFailStyle}>
+        <div className="popup-container" style={popupCongratulationsStyle}>
                 <div className="popup-content" style={popupContentStyle}>
                     <div className="flex items-center flex-col">
                         <Image
@@ -119,6 +119,58 @@ const PopupCongratulations = ({onClose}) => {
 }
 
 
+const PopupSave = ({onClose}) => {
+
+  const popupSaveStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
+  };
+
+  const popupContentStyle = {
+    backgroundColor: 'white',
+    padding: '20px',
+    borderRadius: '10px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+    width: '90%', // Adjust as needed
+  };
+
+  const paragraphStyle = {
+    fontSize: '18px', 
+    fontWeight: 'bold', 
+  };
+
+  return (
+    <div>
+      <div className="popup-container" style={popupSaveStyle}>
+              <div className="popup-content" style={popupContentStyle}>
+                  <div className="flex items-center flex-col">
+                      <Image
+                          src="/icons/congratulations.png"
+                          alt="congratulations"
+                          width={48} 
+                          height={48} 
+                      />
+                      <p>You edit has been saved!</p>
+                      <div className="flex items-center justify-center">
+                          <Button className="mt-5 w-200 h-10" >
+                              <a href='/specialist/found'>Confirm</a>
+                          </Button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+    </div>
+  )
+}
+
 export interface EditItemProps {
   item: {
     name: string;
@@ -136,6 +188,10 @@ export interface EditItemProps {
 const EditItemDescriptionForm: React.FC<EditItemProps> = ({
     item
 }) => {
+
+  const[showPopup, setShowPopup] = useState(false); // pop up message for delete action
+  const[showCongratulations, setShowCongratulations] = useState(false);
+  const[showSave, setShowSave] = useState(false);
 
   const [formFields, setFormFields] = useState({
     itemName: item.name,
@@ -158,13 +214,10 @@ const EditItemDescriptionForm: React.FC<EditItemProps> = ({
     e.preventDefault();
     // Access the form data in the formFields state object
     console.log("Form Data:", formFields);
-    // Perform any other form submission logic here
+    
+    // may need to save data in local storage
   };
 
-
-  // pop up message for delete action
-  const[showPopup, setShowPopup] = useState(false);
-  const[showCongratulations, setShowCongratulations] = useState(false);
   
   const showPopupDelete = () => {
     setShowPopup(true);
@@ -178,142 +231,158 @@ const EditItemDescriptionForm: React.FC<EditItemProps> = ({
     setShowCongratulations(false);
   };
 
+  const handleSaveClose = () => {
+    setShowSave(false);
+  };
+
+
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* <label htmlFor="imageUpload">Item Image</label> */}
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', width: '100%' }}>
-                <Image
-                    alt={item.name}
-                    src={item.imgSrc}
-                    width={250}
-                    height={250}
-                    className="rounded-sm"
-                />
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'flex-end', marginRight: '20px' }}>
-        <button onClick={showPopupDelete}>
-          <Image
-          alt="filterIcon"
-          src="/icons/IconDelete.png"
-          width={30}
-          height={30}/>
-        </button>
-        {showPopup && <PopupDelete onClose={handlePopupClose}/>}
-      </div><br/>
-      <input
-        type="file"
-        id="imageUpload"
-        // onChange={handleImageChange}
-      />
-      <br/>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <input
-          type="text"
-          name="itemName"
-          placeholder={item.name}
-          style={{backgroundColor:"#DCDCDC", borderRadius: '5px'}}
-          onChange={handleInputChange}
-        />
-      </div>
-      <br/>
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-        <div style={{marginLeft: '40px', width: '30%'}}>
-          Location Lost: 
-          <br/>
+    // <div style={{width:"95%"}}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', margin: '0 auto' }}>
+      <form onSubmit={handleSubmit} style={{width: '90%'}}>
+        {/* <label htmlFor="imageUpload">Item Image</label> */}
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', width: '100%' }}>
+                  <Image
+                      alt={item.name}
+                      src={item.imgSrc}
+                      width={250}
+                      height={250}
+                      className="rounded-sm"
+                  />
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'flex-end', marginRight: '20px' }}>
+          <button onClick={showPopupDelete}>
+            <Image
+            alt="filterIcon"
+            src="/icons/IconDelete.png"
+            width={30}
+            height={30}/>
+          </button>
+          {showPopup && <PopupDelete onClose={handlePopupClose}/>}
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
           <input
-              type="text"
-              name="locationLost"
-              placeholder="parking lot"
-              style={{backgroundColor:"#DCDCDC", borderRadius: '5px'}}
-              onChange={handleInputChange}
+            type="text"
+            name="itemName"
+            // placeholder={item.name}
+            style={{backgroundColor:"#DCDCDC", borderRadius: '5px', paddingLeft: '5px', width:'100%'}}
+            value = {item.name}
+            onChange={handleInputChange}
           />
         </div>
-        <div style={{marginRight: '40px'}}>
-          Date Lost: 
-          <br/>
-          <input
-              type="text"
-              name="locationLost"
-              placeholder="parking lot"
-              style={{backgroundColor:"#DCDCDC", borderRadius: '5px'}}
-              onChange={handleInputChange}
-          />
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+          <div style={{width: '45%'}}>
+            Location Lost: 
+            <br/>
+            <input
+                type="text"
+                name="locationLost"
+                placeholder="parking lot"
+                style={{backgroundColor:"#DCDCDC", borderRadius: '5px', paddingLeft: '5px', width:'100%'}}
+                value = {item.location}
+                onChange={handleInputChange}
+            />
+          </div>
+          <div style={{width: '45%'}}>
+            Date Lost: 
+            <br/>
+            <input
+                type="text"
+                name="locationLost"
+                placeholder="Date Lost"
+                style={{backgroundColor:"#DCDCDC", borderRadius: '5px', paddingLeft: '5px', width:'100%'}}
+                value = {item.date}
+                onChange={handleInputChange}
+            />
+              {/* <input
+                id="dateFound"
+                name="dateFound"
+                type="date"
+                style={{backgroundColor:"#DCDCDC", borderRadius: '5px'}}
+                value = {item.date}
+                onChange={handleInputChange}
+              /> */}
+          </div>
         </div>
-      </div>
 
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-        <div style={{marginLeft: '40px', width: '30%'}}>
-          Item Color: 
-          <br/>
-          <select
-              id="itemColor"
-              name="itemColor"
-              className="w-full p-2 border rounded"
-              style={{backgroundColor:"#DCDCDC", borderRadius: '5px'}}
-              onChange={handleInputChange}
-              value={formFields.itemColor}
-          >
-            {COLORS_CATEGORY.map((color) => (
-              <option key={color} value={color}>
-                {color}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div style={{marginRight: '40px'}}>
-          Item Category: 
-          <br/>
-          <select
-              id="itemCategory"
-              name="itemCategory"
-              className="w-full p-2 border rounded"
-              style={{backgroundColor:"#DCDCDC", borderRadius: '5px'}}
-              onChange={handleInputChange}
-              value={formFields.itemCategory}
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '10px' }}>
+          <div style={{width: '45%'}}>
+            Item Color: 
+            <br/>
+            <select
+                id="itemColor"
+                name="itemColor"
+                className="w-full p-2 border rounded"
+                style={{backgroundColor:"#DCDCDC", borderRadius: '5px'}}
+                onChange={handleInputChange}
+                value={formFields.itemColor}
             >
-              {ITEMS_CATEGORY.map((category) => (
-                <option key={category} value={category}>
-                  {category}
+              {COLORS_CATEGORY.map((color) => (
+                <option key={color} value={color}>
+                  {color}
                 </option>
               ))}
             </select>
           </div>
-        </div>
+          <div style={{width: '45%'}}>
+            Item Category: 
+            <br/>
+            <select
+                id="itemCategory"
+                name="itemCategory"
+                className="w-full p-2 border rounded"
+                style={{backgroundColor:"#DCDCDC", borderRadius: '5px'}}
+                onChange={handleInputChange}
+                value={formFields.itemCategory}
+              >
+                {ITEMS_CATEGORY.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
 
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <textarea
-            name="additionalDetails"
-            placeholder="Item details"
-            style={{
-                marginLeft: '10px',
-                height: '150px', 
-                width: '80%', 
-                resize: 'none',
-                backgroundColor: '#DCDCDC',
-                borderRadius: '5px'
-            }}
-            // onChange={handleInputChange}
-        ></textarea>
-      </div>
-      <br/>
-      <div className="flex flex-row justify-between items-center" style={{ marginLeft: '40px', width: '88%' }}>
-        <Button className="w-28 rounded-lg">Save</Button>
-        <div className="flex">
-          <Button className="w-28 rounded-lg">
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+          <textarea
+              name="additionalDetails"
+              placeholder="Item details"
+              style={{
+                  height: '150px', 
+                  width: '100%', 
+                  resize: 'none',
+                  backgroundColor: '#DCDCDC',
+                  borderRadius: '5px',
+                  padding: '5px'
+              }}
+              value={formFields.additionalDetails}
+              onChange={handleInputChange}
+          ></textarea>
+        </div>
+        <br/>
+        <div className="flex flex-row justify-between items-center" style={{ display: 'flex', justifyContent: 'center'}}>
+          <Button onClick={() => setShowSave(true)} 
+            className="w-40 rounded-lg">Save
+          </Button>
+          {showSave && <PopupSave onClose={handleSaveClose}  />}
+
+          <Button className="w-40 rounded-lg" style={{ marginLeft: 'auto' }}>
             <a href="/specialist/found">Cancel</a>
           </Button>
         </div>
-      </div>
+        
+        <br/>
+        <div className="pb-20 flex flex-column items-center justify-center">
+          <a onClick={() => setShowCongratulations(true)}>
+            <p style={{ color: 'blue', textDecoration: 'underline' }}>Report as Found</p>
+          </a>
+          {showCongratulations && <PopupCongratulations onClose={handleCongratulationsClose}  />}
+        </div>
 
-      <div className="pb-20 flex flex-column items-center justify-center">
-        <a onClick={() => setShowCongratulations(true)}>
-          <p style={{ color: 'blue', textDecoration: 'underline' }}>Report as Found</p>
-        </a>
-        {showCongratulations && <PopupCongratulations onClose={handleCongratulationsClose}  />}
-      </div>
-
-    </form>
+      </form>
+    </div>
   );
   
 };
