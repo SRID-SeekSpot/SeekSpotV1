aa88f0d340d575e13e03b3479a2d6e4b87a30e91
Kejie He <kejieh@andrew.cmu.edu>
2023-12-05 21:52:52 -0800

    Type Hinting

--- app/non-specialist/[id]/page.tsx
+++ app/non-specialist/[id]/page.tsx
@@ -5,39 +5,36 @@ import { ALL_ITEMS } from "@/app/constants/AllItems";
 import { useEffect, useState } from "react";
 import LostItemDetailPage_nonspecialist from "@/app/non-specialist/lostItemDetail_nonspecialist/page";
 import FoundItemDetailPage_nonspecialist from "@/app/non-specialist/foundItemDetail_nonspecialist/page";
+import { ALL_ITEM_TYPES } from "@/types/AllItemTypes";
 
 const ItemDetail = () => {
     const path = usePathname();
-    const id = path.split("/").pop();
-    const [foundItemList, setFoundItemList] = useState(ALL_ITEMS);
-    const [item, setItem] = useState(() => foundItemList.find((item) => item.id === id));
+    const id: string | undefined = path.split("/").pop();
 
-    // useEffect(() => {
-    //     // Fetch data from localStorage only once when the component mounts
-    //     let updatedBountyItemsString = localStorage.getItem("updatedBountyItems");
-
-    //     if (updatedBountyItemsString !== "null") {
-    //         let updatedBountyItems = JSON.parse(updatedBountyItemsString);
-    //         setFoundItemList(updatedBountyItems);
-    //         const newItem = updatedBountyItems.find((item) => item.id === id);
-    //         setItem(newItem);
-    //     }
-    // }, []);
+    const [foundItemList, setFoundItemList] =
+        useState<ALL_ITEM_TYPES[]>(ALL_ITEMS);
+    const [item, setItem] = useState<ALL_ITEM_TYPES | undefined>(() =>
+        foundItemList.find((item) => item.id === id)
+    );
 
     useEffect(() => {
-        const updatedBountyItemsString = localStorage.getItem("updatedBountyItems");
-    
+        const updatedBountyItemsString =
+            localStorage.getItem("updatedBountyItems");
+
         if (updatedBountyItemsString && updatedBountyItemsString !== "null") {
-            const updatedBountyItems = JSON.parse(updatedBountyItemsString);
+            const updatedBountyItems: ALL_ITEM_TYPES[] = JSON.parse(
+                updatedBountyItemsString
+            );
             setFoundItemList(updatedBountyItems);
-            const newItem = updatedBountyItems.find((item) => item.id === id);
+            const newItem: ALL_ITEM_TYPES | undefined = updatedBountyItems.find(
+                (updatedItem: ALL_ITEM_TYPES) => updatedItem.id === id
+            );
             setItem(newItem);
         }
     }, [id]); // Add id as a dependency
-    
 
     // If item is not found
-    if (!item || !id) {
+    if (item === undefined || id === undefined) {
         return <p>Item not found!</p>;
     }
 
---
+++ types/AllItemTypes.ts
@@ -0,0 +1,13 @@
+export interface ALL_ITEM_TYPES {
+    id: string;
+    name: string;
+    imgSrc: string;
+    description: string;
+    price: number;
+    color: string;
+    category: string;
+    location: string;
+    date: string;
+    claimCode: string;
+    claimStatus: boolean;
+}
